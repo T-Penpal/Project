1c30d1d16b4f39f0d7c954a1fcdfc1daa020e80a
JYuRan <ruse216@gmail.com>
2022-11-02 10:55:42 +0900

    임시 수정(로그아웃시 정상작동여부 확인 필요)

--- src/main/java/com/penpal/project/board/BoardController.java
+++ src/main/java/com/penpal/project/board/BoardController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,6 +26,8 @@ import com.penpal.project.list.LanguageList;
 import com.penpal.project.list.LanguageListRepository;
 import com.penpal.project.list.LocationList;
 import com.penpal.project.list.LocationListRepository;
+import com.penpal.project.member.Member;
+import com.penpal.project.member.MemberService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,7 +39,7 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardController {
 
 	private final BoardService boardService;
-	/* private final MemberService memberService; */
+	private final MemberService memberService; 
 	private final CategoryListRepository categoryListRepository;
 	private final LocationListRepository locationListRepository;
 	private final CountryListRepository countryListRepository;
@@ -76,27 +79,27 @@ public class BoardController {
 	}
 
 	// by 장유란, 답변기능 권한 주석처리/**/
-	// @PreAuthorize("isAuthenticated()") // 로그인 제약
+	@PreAuthorize("isAuthenticated()") // 로그인 제약
 	@GetMapping("/create")
 	public String boardCreate(BoardForm boardForm) {
 		return "community/writeForm";
 	}
 
 	// by 장유란, 답변기능 권한 주석처리
-	/* @PreAuthorize("isAuthenticated()") */
+	@PreAuthorize("isAuthenticated()")
 	@PostMapping("/create")
-	public String boardCreate(@Valid BoardForm boardForm, BindingResult bindingResult/* , Principal principal */) {
+	public String boardCreate(@Valid BoardForm boardForm, BindingResult bindingResult, Principal principal) {
 		if (bindingResult.hasErrors()) {
 			return "community/writeForm";
 		}
-		/* Member member = this.memberService.getMember(principal.getName()); */
+		Member member = this.memberService.getMember(principal.getName());
 		this.boardService.create(boardForm.getTitle(), boardForm.getContent(), boardForm.getCategory(),
 				boardForm.getLocation(), boardForm.getCountry()/* , member */);
 
 		return "redirect:/community"; // by 장유란, "redirect:community/community"; ==> "redirect:/community";
 	}
 
-	/* @PreAuthorize("isAuthenticated()") */
+	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/modify/{id}")
 	public String boardModify(BoardForm boardForm, @PathVariable("id") Integer id, Principal principal) {
 		Board board = this.boardService.getBoard(id);
@@ -112,7 +115,7 @@ public class BoardController {
 
 	@PostMapping("/modify/{id}")
 	public String boardModify(@Valid BoardForm boardForm,
-			BindingResult bindingResult/*																	 */,
+			BindingResult bindingResult,
 			@PathVariable("id") Integer id) {
 		if (bindingResult.hasErrors()) {
 			return "community/writeForm";
