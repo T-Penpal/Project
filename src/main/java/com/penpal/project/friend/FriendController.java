70c55432fec87189bbc855ea88d520cb5d889433
juneon <tly013@naver.com>
2022-11-07 16:31:56 +0900

    friend 기능 수정중

--- src/main/java/com/penpal/project/friend/FriendController.java
+++ src/main/java/com/penpal/project/friend/FriendController.java
@@ -30,6 +30,24 @@ public class FriendController {
 		return friendList;
 	}
    
+   // by 안준언, 접속중인 친구 리스트 가져오기 (임시)
+   @RequestMapping("/getOnlineFriend")
+   @ResponseBody
+   public List<Friend> getOnlineFriend(@RequestParam HashMap<Object, Object> params){
+	   	Member member = this.memberService.getMember(Integer.parseInt((String) params.get("memberId")));
+		List<Friend> friendList = new ArrayList<>();
+		friendList.addAll(member.getFriendList());
+		List<Friend> onlineFriendList = new ArrayList<>();
+		
+		for(int i=0; i<friendList.size(); i++) {
+			if(friendList.get(i).getFriend().isConn() == true) {
+				onlineFriendList.add(friendList.get(i));
+			}
+		}
+		
+		return onlineFriendList;
+   }
+   
    // by 안준언, 내가 받은 친구 요청 리스트 가져오기 (임시)
    @RequestMapping("/getFriendRequest")
    @ResponseBody
